00ca0fc779b79fcb2a934b27476a3f195bc0047d
Hendrik Obermayer <hendrik@comolo.de>
2013-09-19 15:22:20 +0200

    removed debug code

--- system/modules/theme_external_js/classes/GenerateCoffeescript.php
+++ system/modules/theme_external_js/classes/GenerateCoffeescript.php
@@ -44,7 +44,7 @@ class GenerateCoffeescript extends AssetGenerator
 		// Target File
 		$strJSFile = 'assets/js/coffee-'.md5_file($strSourcePath).'.js';
 		
-		if(!file_exists(TL_ROOT.'/'.$strJSFile) || true) {
+		if(!file_exists(TL_ROOT.'/'.$strJSFile)) {
 			
 			// require classes
 			require_once __DIR__.'/../vendor/coffeescript/coffeescript/src/CoffeeScript/Init.php';
