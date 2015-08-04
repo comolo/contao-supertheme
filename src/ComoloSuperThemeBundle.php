4390ce1a38a6d2f4bafe76c6ad49420e4274b7d0
Hendrik Obermayer <hendrik@comolo.de>
2015-08-04 14:07:52 +0200

    added symfony bundle class

---
+++ src/ComoloSuperThemeBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the SuperTheme extension by Comolo
+ *
+ * Copyright (C) 2013-2015 Hendrik Obermayer
+ *
+ * @package   SuperTheme
+ * @author    Hendrik Obermayer - Comolo GmbH <mail@comolo.de>
+ * @copyright 2015 - Hendrik Obermayer - Comolo GmbH <mail@comolo.de>
+ * @license   LGPL
+ */
+
+namespace Comolo\SuperThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the SuperTheme bundle.
+ *
+ * @author    Hendrik Obermayer - Comolo GmbH <mail@comolo.de>
+ */
+class ComoloSuperThemeBundle extends Bundle
+{
+}
